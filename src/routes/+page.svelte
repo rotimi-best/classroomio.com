5c7d2dde672a4a6758d109fea5ca1e8c1df79c5b
Rotimi Best <best@classroomio.com>
2023-09-04 17:21:38 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<h1 class="text-3xl font-bold underline">Welcome to classroomIO</h1>
+
+<style lang="postcss">
+  /* Change this to what it needs to be */
+  :global(html) {
+    background-color: theme(colors.blue.100);
+  }
+</style>
