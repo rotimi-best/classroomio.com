a4359eab08f0eba28b87beb9ee21be9debb3e347
tunny17 <tvnji17@gmail.com>
2023-09-25 12:22:28 +0100

    CLASSROOMIO-T-176: Icon Rename

--- src/lib/emojis/index.js
+++ src/lib/emojis/index.js
@@ -1,13 +1,13 @@
-import robotArm from './mechanical-arm-svgrepo-com.svg';
-import thumbsUp from './thumbs-up-skin-3-svgrepo-com.svg';
-import flexible from './crossed-fingers-skin-3-svgrepo-com (1).svg';
-import rightFist from './left-facing-fist-skin-3-svgrepo-com.svg';
-import leftFist from './right-facing-fist-skin-3-svgrepo-com.svg';
+import robotArm from './robot-arm.svg';
+import thumbsUp from './thumbs-up.svg';
+import flexible from './crossed-fingers.svg';
+import rightFist from './left-fist.svg';
+import leftFist from './right-fist.svg';
 
-import globe from './globe-world-earth-svgrepo-com.svg';
-import team from './leadership-team-group-svgrepo-com.svg';
-import transcription from './printer-print-printing-svgrepo-com.svg';
-import folder from './folder-file-document-svgrepo-com.svg';
+import globe from './globe.svg';
+import team from './team.svg';
+import transcription from './printer.svg';
+import folder from './folder.svg';
 
 export {
   flexible,
